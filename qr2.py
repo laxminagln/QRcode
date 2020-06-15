43503628164709cfca99400025906c26f38a8bbe
nagln <laxminagln@gmail.com>
2020-06-15 21:11:28 +0530

    Update qr2.py

--- qr2.py
+++ qr2.py
@@ -1,1 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 15 21:15:21 2020
+@author: nagln
+"""
 
+import qrcode
+qr = qrcode.QRCode(
+    version = 1,
+    box_size = 10,
+    border = 5
+    )
+
+data = input()
+qr.add_data(data)
+qr.make(fit = True)
+img = qr.make_image(fill = "black", back_color = "white")
+img.save("1.png")
